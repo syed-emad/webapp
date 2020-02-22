7c5ee5b452782834ab586d5c84f797b0c60bca92
syed-emad <61075900+syed-emad@users.noreply.github.com>
2020-02-22 21:13:21 +0500

    Login System

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ export class App extends Component {
           <Route exact path="/" component={HeaderM}></Route>
           <Route exact path="/" component={SlideM}></Route>
           <Route exact path="/" component={PopularM}></Route>
+          <Route exact path="/" component={SlideM}></Route>
+          <Route exact path="/" component={SlideM}></Route>
           <Route exact path="/" component={Testimonials}></Route>
           <Route exact path="/" component={FooterX}></Route>
           <Route exact path="/Popular.js" component={Popular}></Route>
