3bb54a29083adf7e07a7324c7ea596c174b3f5ea
syed-emad <61075900+syed-emad@users.noreply.github.com>
2020-03-06 00:44:54 +0500

    Making Login Session using passport

--- Login/routes/users.js
+++ Login/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 //User model
 const User = require("../models/Users");
@@ -79,4 +80,12 @@ router.post("/register", (req, res) => {
   }
 });
 
+//Login Handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })(req, res, next);
+});
 module.exports = router;
