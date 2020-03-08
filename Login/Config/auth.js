428ddae86b15a27da5352ebccd211ea68efd7a7c
syed-emad <61075900+syed-emad@users.noreply.github.com>
2020-03-08 06:57:04 +0500

    Login Created Again
    All branches got ruined

--- Login/Config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  ensureAuthenticated: function(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    req.flash("error_msg", "Pleas log in to view this resourcse");
-    res.redirect("/users/login");
-  }
-};
--- Login/Config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MongoURI:
-    "mongodb://emad:emad@cluster0-shard-00-00-cguh9.mongodb.net:27017,cluster0-shard-00-01-cguh9.mongodb.net:27017,cluster0-shard-00-02-cguh9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-};
