079241fa22a2d643cca0a6097a0bfe90f9fa02ac
neha-ahmed <61065291+neha-ahmed@users.noreply.github.com>
2020-03-08 13:11:39 +0500

    bs3

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,11 +6,27 @@ const router=   express.Router();        //sub pckg of express to handle diff ro
 
 router.get('/', (req, res, next) => {
     Product.find()
+    .select('name price _id')  
     .exec()
     .then(docs => {
-        console.log(docs);
+        const response={                   //to have only relevant info displayed
+            count: docs.length, 
+            product: docs.map(doc =>{        
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id:doc._id,
+                    request: {                //extra info that u want to display
+                        type:'GET',
+                        url: 'http://localhost:3001/products/'+ doc._id
+
+                    }
+                }
+            })
+        } 
+        //console.log(docs);
         // if(docs.length >=0){
-            res.status(200).json(docs);
+            res.status(200).json(response);
         // } else{
         //     res.status(404).json({
         //         message:'empty array'
@@ -29,12 +45,21 @@ router.get('/', (req, res, next) => {
       router.get('/:prodID', (req, res, next) => {
         const id= req.params.prodID;
         Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log("from database",doc);
             
             if(doc){
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product:doc,
+                    request:{
+                        type:'GET',
+                        description: 'GET_ALL_PRODUCTS',
+                        url: 'http://localhost:3001/products/'
+
+                    }
+                });
             }
             else{
                 res.status(404).json({message :'No valid entry found in database'});
@@ -60,8 +85,17 @@ router.get('/', (req, res, next) => {
           .then(result =>{
               console.log(result);
               res.status(201).json ({
-                message: 'handling POST reqs',
-                createdProduct: result
+                message: 'created product sucessfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {                //extra info that u want to display
+                        type:'GET',
+                        url: 'http://localhost:3001/products/'+ result._id
+
+                    }
+                }
             });
           })
           .catch(err => {
@@ -83,7 +117,13 @@ router.get('/', (req, res, next) => {
         Product.update({_id:id}, {$set : updateOps})
         .exec()
         .then(result =>{
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request:{
+                    type:'GET',
+                    url: 'http://localhost:3001/products/'+ id
+                }
+            });
         })
         .catch(err => {
             console.log(err); 
@@ -103,7 +143,14 @@ router.get('/', (req, res, next) => {
         Product.remove({_id:id})
        .exec()
        .then(result =>{
-           res.status(200).json(result);
+        res.status(200).json({
+            message:'product deleted',
+            request:{
+             type: 'POST',
+             url:'http://localhost:3001/products',
+             body:{name :'String', price: 'Number'}
+            }
+        });
        })
        .catch(err => {
            console.log(err); 
