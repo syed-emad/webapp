e029b3693620e3868a5e721ba01e68874d5195b4
neha-ahmed <61065291+neha-ahmed@users.noreply.github.com>
2020-03-09 23:52:30 +0500

    new

--- client/app/components/App/App.js
+++ client/app/components/App/App.js
@@ -3,32 +3,26 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
+import HeaderM from "../MainPage/HeaderM";
+import Slider, { SlideM } from "../MainPage/SlideM";
+import PopularM from "../MainPage/PopularM";
+import ServiceX from "../MainPage/ServiceX";
+import Testimonials from "../MainPage/Testimonials";
+import FooterX from "../MainPage/FooterX";
+import Home from "../Home/Home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import HeaderM from "../HeaderM";
-import Slider, { SlideM } from "../SlideM";
-import PopularM from "../PopularM";
-import Testimonials from "../Testimonials";
-import FooterX from "../FooterX";
-
-
 const App = ({ children }) => (
   <>
-    
-     <div>
-        <div class="super_container">
-          <Route exact path="/" component={HeaderM}></Route>
-          <Route exact path="/" component={SlideM}></Route>
-          <Route exact path="/" component={PopularM}></Route>
-          <Route exact path="/" component={Testimonials}></Route>
-          <Route exact path="/" component={FooterX}></Route>
-
-          <Route exact path="/" component={Header}></Route>
-          <main> {children}</main>
-          <Route exact path="/" component={Footer}></Route>
-          
-        </div>
-      </div>
+    <div class="super_contianer">
+      <Route exact path="/" component={HeaderM}></Route>
+      <Route exact path="/" component={SlideM}></Route>
+      <Route exact path="/" component={PopularM}></Route>
+      <Route exact path="/" component={SlideM}></Route>
+      <Route exact path="/" component={SlideM}></Route>
+      <Route exact path="/" component={Testimonials}></Route>
+      <Route exact path="/" component={FooterX}></Route>
+      <Route exact path="/Home" component={Home}></Route>
+    </div>
   </>
 );
 
--- config/config.js
+++
@@ -1,8 +0,0 @@
- // Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  db:
-    "mongodb+srv://emad:emad@cluster0-cguh9.mongodb.net/test?retryWrites=true&w=majority",
-  db_dev:
-    "mongodb+srv://emad:emad@cluster0-cguh9.mongodb.net/test?retryWrites=true&w=majority"
-};
