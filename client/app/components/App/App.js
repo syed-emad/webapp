0759cc0fddb1d87d21345804992557af314740e7
syed-emad <61075900+syed-emad@users.noreply.github.com>
2020-03-09 15:16:15 +0500

    Login+MERN

--- client/app/components/App/App.js
+++ client/app/components/App/App.js
@@ -3,13 +3,26 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
+import HeaderM from "../MainPage/HeaderM";
+import Slider, { SlideM } from "../MainPage/SlideM";
+import PopularM from "../MainPage/PopularM";
+import ServiceX from "../MainPage/ServiceX";
+import Testimonials from "../MainPage/Testimonials";
+import FooterX from "../MainPage/FooterX";
+import Home from "../Home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const App = ({ children }) => (
   <>
-    <Header />
-
-    <main>{children}</main>
-
-    <Footer />
+    <div class="super_contianer">
+      <Route exact path="/" component={HeaderM}></Route>
+      <Route exact path="/" component={SlideM}></Route>
+      <Route exact path="/" component={PopularM}></Route>
+      <Route exact path="/" component={SlideM}></Route>
+      <Route exact path="/" component={SlideM}></Route>
+      <Route exact path="/" component={Testimonials}></Route>
+      <Route exact path="/" component={FooterX}></Route>
+      <Route exact path="/Home" component={Home}></Route>
+    </div>
   </>
 );
 
---
+++ client/app/components/MainPage/FooterX.js
@@ -0,0 +1,182 @@
+import React, { Component } from "react";
+
+export class FooterX extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="footer">
+          <div class="container">
+            <div class="newsletter">
+              <div class="row">
+                <div class="col">
+                  <div class="section_title text-center">
+                    <h1>Subscribe to newsletter</h1>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col text-center">
+                  <div class="newsletter_form_container mx-auto">
+                    <form action="post">
+                      <div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-center">
+                        <input
+                          id="newsletter_email"
+                          class="newsletter_email"
+                          type="email"
+                          placeholder="Email Address"
+                          required="required"
+                          data-error="Valid email is required."
+                        />
+                        <button
+                          id="newsletter_submit"
+                          type="submit"
+                          class="newsletter_submit_btn trans_300"
+                          value="Submit"
+                        >
+                          Subscribe
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="footer_content">
+              <div class="row">
+                <div class="col-lg-3 footer_col">
+                  <div class="logo_container">
+                    <div class="logo">
+                      <img src="images/bulb3.png" alt="" />
+                      <span>Professor</span>
+                    </div>
+                  </div>
+
+                  <p class="footer_about_text">
+                    In aliquam, augue a gravida rutrum, ante nisl fermentum
+                    nulla, vitae tempor nisl ligula vel nunc. Proin quis mi
+                    malesuada, finibus tortor fermentum, tempor lacus.
+                  </p>
+                </div>
+
+                <div class="col-lg-3 footer_col">
+                  <div class="footer_column_title">Menu</div>
+                  <div class="footer_column_content">
+                    <ul>
+                      <li class="footer_list_item">
+                        <a href="#">Home</a>
+                      </li>
+                      <li class="footer_list_item">
+                        <a href="#">About Us</a>
+                      </li>
+                      <li class="footer_list_item">
+                        <a href="courses.html">Courses</a>
+                      </li>
+                      <li class="footer_list_item">
+                        <a href="news.html">News</a>
+                      </li>
+                      <li class="footer_list_item">
+                        <a href="contact.html">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 footer_col">
+                  <div class="footer_column_title">Usefull Links</div>
+                  <div class="footer_column_content">
+                    <ul>
+                      <li class="footer_list_item">
+                        <a href="#">Testimonials</a>
+                      </li>
+                      <li class="footer_list_item">
+                        <a href="#">FAQ</a>
+                      </li>
+
+                      <li class="footer_list_item">
+                        <a href="#">Tuitions</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 footer_col">
+                  <div class="footer_column_title">Contact</div>
+                  <div class="footer_column_content">
+                    <ul>
+                      <li class="footer_contact_item">
+                        <div class="footer_contact_icon">
+                          <img
+                            src="images/placeholder.svg"
+                            alt="https://www.flaticon.com/authors/lucy-g"
+                          />
+                        </div>
+                        Blvd Libertad, 34 m05200 Arévalo
+                      </li>
+                      <li class="footer_contact_item">
+                        <div class="footer_contact_icon">
+                          <img
+                            src="images/smartphone.svg"
+                            alt="https://www.flaticon.com/authors/lucy-g"
+                          />
+                        </div>
+                        0034 37483 2445 322
+                      </li>
+                      <li class="footer_contact_item">
+                        <div class="footer_contact_icon">
+                          <img
+                            src="images/envelope.svg"
+                            alt="https://www.flaticon.com/authors/lucy-g"
+                          />
+                        </div>
+                        hello@company.com
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="footer_bar d-flex flex-column flex-sm-row align-items-center">
+              <div class="footer_copyright">
+                <span></span>
+              </div>
+              <div class="footer_social ml-sm-auto">
+                <ul class="menu_social">
+                  <li class="menu_social_item">
+                    <a href="#">
+                      <i class="fab fa-pinterest"></i>
+                    </a>
+                  </li>
+                  <li class="menu_social_item">
+                    <a href="#">
+                      <i class="fab fa-linkedin-in"></i>
+                    </a>
+                  </li>
+                  <li class="menu_social_item">
+                    <a href="#">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li class="menu_social_item">
+                    <a href="#">
+                      <i class="fab fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li class="menu_social_item">
+                    <a href="#">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default FooterX;
